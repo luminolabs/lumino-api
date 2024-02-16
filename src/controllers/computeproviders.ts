353d21f1192c99d18b3140e4b78dd63ad712f01b
yogeshdarji <yogeshdarji913@gmail.com>
2024-02-16 04:21:28 -0800

    Add new fields to compute provider

--- src/controllers/computeproviders.ts
+++ src/controllers/computeproviders.ts
@@ -1,69 +1,108 @@
-import express from 'express';
+import express from "express";
 
-import { createComputeProvider, deleteComputeProviderById, getComputeProviderById, getComputeProviders } from '../db/computeprovider';
+import {
+  createComputeProvider,
+  deleteComputeProviderById,
+  getComputeProviderById,
+  getComputeProviders,
+} from "../db/computeprovider";
 
-export const getAllComputeProviders = async (req: express.Request, res: express.Response) => {
-    try {
-        const computeproviders = await getComputeProviders();
+export const getAllComputeProviders = async (
+  req: express.Request,
+  res: express.Response
+) => {
+  try {
+    const computeproviders = await getComputeProviders();
 
-        return res.status(200).json(computeproviders);
-    } catch (error) {
-        console.log(error);
-        return res.sendStatus(400);
-    }
+    return res.status(200).json(computeproviders);
+  } catch (error) {
+    console.log(error);
+    return res.sendStatus(400);
+  }
 };
 
-export const createNewComputeProvider = async (req: express.Request, res: express.Response) => {
-    try {
-  
-      const { username, email, name, resources, available, ipaddress } = req.body;
+export const createNewComputeProvider = async (
+  req: express.Request,
+  res: express.Response
+) => {
+  try {
+    const {
+      username,
+      email,
+      name,
+      gpu,
+      memory,
+      total_count_of_gpus,
+      count_of_gpus_per_sever,
+      storage_capacity,
+      nvidia_interlink,
+      cpu_model,
+      gpu_availaible_start_date,
+      gpu_availaible_end_date,
+      available,
+      location,
+    } = req.body;
 
-      const computeProvider = await createComputeProvider({
-        email,
-        username,
-        name,
-        resources,
-        available,
-        ipaddress
-      });
-      
-      return res.status(200).json(computeProvider).end();
-    } catch (error) {
-      console.log(error);
+    const computeProvider = await createComputeProvider({
+      email,
+      username,
+      name,
+      gpu,
+      memory,
+      total_count_of_gpus,
+      count_of_gpus_per_sever,
+      storage_capacity,
+      nvidia_interlink,
+      cpu_model,
+      gpu_availaible_start_date,
+      gpu_availaible_end_date,
+      available,
+      location,
+    });
+
+    return res.status(200).json(computeProvider).end();
+  } catch (error) {
+    console.log(error);
+    return res.sendStatus(400);
+  }
+};
+
+export const deleteComputeProvider = async (
+  req: express.Request,
+  res: express.Response
+) => {
+  try {
+    const { id } = req.params;
+
+    const deletedComputeProvider = await deleteComputeProviderById(id);
+
+    return res.json(deletedComputeProvider);
+  } catch (error) {
+    console.log(error);
+    return res.sendStatus(400);
+  }
+};
+
+export const updateComputeProvider = async (
+  req: express.Request,
+  res: express.Response
+) => {
+  try {
+    const { id } = req.params;
+    const { username } = req.body;
+
+    if (!username) {
       return res.sendStatus(400);
     }
+
+    const user = await getComputeProviderById(id);
+
+    user.username = username;
+    await user.save();
+
+    return res.status(200).json(user).end();
+  } catch (error) {
+    console.log(error);
+    return res.sendStatus(400);
   }
-
-export const deleteComputeProvider = async (req: express.Request, res: express.Response) => {
-    try {
-        const { id } = req.params;
-
-        const deletedComputeProvider = await deleteComputeProviderById(id);
-
-        return res.json(deletedComputeProvider);
-    } catch (error) {
-        console.log(error);
-        return res.sendStatus(400);
-    }
-}
-
-export const updateComputeProvider = async (req: express.Request, res: express.Response) => {
-    try {
-      const { id } = req.params;
-      const { username } = req.body;
-  
-      if (!username) {
-        return res.sendStatus(400);
-      }
-  
-      const user = await getComputeProviderById(id);
-      
-      user.username = username;
-      await user.save();
-  
-      return res.status(200).json(user).end();
-    } catch (error) {
-      console.log(error);
-      return res.sendStatus(400);
-    }
-  }
+};
--- src/db/computeprovider.ts
+++ src/db/computeprovider.ts
@@ -4,9 +4,17 @@ const ComputeProviderSchema = new mongoose.Schema({
     username: { type: String, required: true },
     email: { type: String, required: true },
     name: { type: String, required: true },
-    resources: { type: String, required: true },
+    gpu: { type: String, required: true },
+    memory: { type: String, required: true },
+    total_count_of_gpus: { type: String, required: true },
+    count_of_gpus_per_sever: { type: String, required: true},
+    storage_capacity: { type: String, required: true },
+    nvidia_interlink: { type: Boolean, required: true },
+    cpu_model: { type: String, required: true },
+    gpu_availaible_start_date: { type: String, required: true },
+    gpu_availaible_end_date: { type: String, required: true },
     available: { type: Boolean, required: true },
-    ipaddress: { type: String, required: true }
+    location: { type: String, required: true }
 });
 
 export const ComputeProviderModel = mongoose.model('ComputeProvider', ComputeProviderSchema);
