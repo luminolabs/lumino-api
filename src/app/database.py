80449dc5c0d0c4df9e3c786bc66924497a45926e
Vasilis Raptakis <scaraveos@gmail.com>
2024-08-21 17:33:58 -0700

    Minor changes

--- src/app/database.py
+++ src/app/database.py
@@ -3,15 +3,21 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 from app.config_manager import config
 
+# Create the database engine
 engine = create_async_engine(config.database_url, echo=config.sqlalchemy_log_all)
 AsyncSessionLocal = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
-
 Base = declarative_base()
 
 
 async def get_db():
+    """
+    Async context manager to handle database sessions.
+    This function can be used as a FastAPI dependency to get a database session.
+    """
     async with AsyncSessionLocal() as db:
         try:
+            # Yield the database session to the function that depends on it
             yield db
         finally:
+            # Ensure the session is closed after the function finishes
             await db.close()
