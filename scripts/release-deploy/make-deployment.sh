637e59e77fd5907397701d52f7dbbfff9526644a
Vasilis Raptakis <scaraveos@gmail.com>
2024-10-08 15:09:19 -0700

    LUM-841: Automate deployments (#33)

---
+++ scripts/release-deploy/make-deployment.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# This script is used to orchestrate the deployment process
+# Run this script locally, before running deploy-to-mig.sh
+
+# Import common functions and variables
+source ./scripts/utils.sh
+
+gcloud compute ssh $BUILD_VM --project=$PROJECT_ID --zone=$WORK_ZONE \
+      --command="SERVICE_NAME=$SERVICE_NAME /$SERVICE_NAME/scripts/release-deploy/_git-pull.sh"
+gcloud compute ssh $BUILD_VM --project=$PROJECT_ID --zone=$WORK_ZONE \
+      --command="SERVICE_NAME=$SERVICE_NAME /$SERVICE_NAME/scripts/release-deploy/_make-docker-image.sh"
