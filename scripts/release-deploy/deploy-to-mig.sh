8a49c75e08f3289f484dd81777a9e4c03ea1f578
Vasilis Raptakis <scaraveos@gmail.com>
2024-10-08 18:00:54 -0700

    LUM-841: Automated deployments (fix)

--- scripts/release-deploy/deploy-to-mig.sh
+++ scripts/release-deploy/deploy-to-mig.sh
@@ -6,15 +6,17 @@
 # Import common functions and variables
 source ./scripts/utils.sh  # Sets $SERVICE_NAME
 
+echo "About to delete instances in the MIG group: $SERVICE_NAME-prod. Instances will be recreated automatically."
+
 # Get the list of MIG VMs
 VMS=$(gcloud compute instance-groups managed list-instances $SERVICE_NAME-prod --project=$PROJECT_ID --zone=$WORK_ZONE --format="value(name)")
 
 # Loop through each VM
 for VM in $VMS; do
     echo "Processing VM: $VM"
-    # SSH into the VM, change directory, run the script, and exit
-    gcloud compute ssh $VM --project=$PROJECT_ID --zone=$WORK_ZONE \
-      --command="SERVICE_NAME=$SERVICE_NAME /$SERVICE_NAME/scripts/mig-runtime/start-services.sh --force-recreate && exit"
+    gcloud compute instance-groups managed delete-instances $SERVICE_NAME-prod \
+      --project=$PROJECT_ID --zone=$WORK_ZONE \
+      --instances=$VM --quiet
     echo "Completed processing VM: $VM"
 done
 
