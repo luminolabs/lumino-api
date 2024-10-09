4544327baf48428d1854f9ae3db7beceda6d143a
Vasilis Raptakis <scaraveos@gmail.com>
2024-10-08 17:49:19 -0700

    LUM-841: Automated deployments (fix)

--- scripts/release-deploy/make-deployment.sh
+++ scripts/release-deploy/make-deployment.sh
@@ -3,6 +3,8 @@
 # This script is used to orchestrate the deployment process
 # Run this script locally, before running deploy-to-mig.sh
 
+# NOTE: You need your github private key in ~/.ssh/id_rsa in the Scheduler VM to run this script
+
 # Import common functions and variables
 source ./scripts/utils.sh  # Sets $SERVICE_NAME
 
