68f77b562d4b569822cc7d3ea12c59872a0a632d
Vasilis Raptakis <scaraveos@gmail.com>
2024-10-09 10:56:55 -0700

    LUM-841: Automated deployments (fix)

--- scripts/release-deploy/deploy-to-mig.sh
+++ scripts/release-deploy/deploy-to-mig.sh
@@ -10,15 +10,17 @@ echo "About to delete instances in the MIG group: $SERVICE_NAME-prod. Instances 
 
 # Get the list of MIG VMs
 VMS=$(gcloud compute instance-groups managed list-instances $SERVICE_NAME-prod \
-  --project=$PROJECT_ID --zone=$WORK_ZONE --format="value(name)")
+  --project=$PROJECT_ID --zone=$WORK_ZONE \
+  --filter="instanceStatus=RUNNING AND currentAction=NONE" \
+  --format="value(name)")
 
 # Loop through each VM
 for VM in $VMS; do
-    echo "Processing VM: $VM"
+    echo "Deleting VM: $VM"
     gcloud compute instance-groups managed delete-instances $SERVICE_NAME-prod \
       --project=$PROJECT_ID --zone=$WORK_ZONE \
       --instances=$VM --quiet || true
-    echo "Completed processing VM: $VM"
+    echo "Completed deleting VM: $VM"
 done
 
 echo "Done."
