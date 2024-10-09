5c2ab7404055a5ec319a4b25046d3e9a3f34d2bb
Vasilis Raptakis <scaraveos@gmail.com>
2024-10-09 16:41:07 -0700

    LUM-841: Automated deployments

--- scripts/release-deploy/deploy-to-mig.sh
+++ scripts/release-deploy/deploy-to-mig.sh
@@ -20,30 +20,23 @@ else
   echo "No version provided, using the latest image tag."
 fi
 
+# Get the current list of VMs before the update (Old VMs)
+OLD_VMS=$(gcloud compute instance-groups managed list-instances $SERVICE_NAME-prod \
+  --project=$PROJECT_ID --zone=$WORK_ZONE \
+  --format="value(name)")
+
+# Get the current MIG target size (the desired number of instances)
+TARGET_SIZE=$(gcloud compute instance-groups managed describe $SERVICE_NAME-prod \
+  --project=$PROJECT_ID --zone=$WORK_ZONE \
+  --format="value(targetSize)")
+
 # Start the rolling update
-echo "Starting the rolling update... This may take a few minutes."
+echo "Starting the rolling update."
 # Flags:
 # --max-unavailable=0: Our minimum number of instances is 1, so we can't have any unavailable
 # --min-ready=60s: Wait for 60 seconds after an instance is ready before considering it available
 gcloud beta compute instance-groups managed rolling-action replace $SERVICE_NAME-prod \
   --project=$PROJECT_ID --zone=$WORK_ZONE \
   --max-unavailable=0 --min-ready=${BUILD_DURATION}s > /dev/null
-
-# Monitor the status of the rolling update
-echo "Rolling update started. Monitoring update progress..."
-while true; do
-  STATUS=$(gcloud compute instance-groups managed describe $SERVICE_NAME-prod \
-    --project=$PROJECT_ID --zone=$WORK_ZONE \
-    --format="value(status.isStable)")
-  # Check if the rolling update is complete
-  if [[ "$STATUS" == "True" ]]; then
-    echo "Rolling update completed successfully."
-    break
-  else
-    echo "Rolling update in progress. Waiting for completion..."
-  fi
-  # Wait for a few seconds before checking again
-  sleep 10
-done
-
+echo "Rolling update started and *this script will exit now* - it may take a few minutes for the update to complete."
 echo "Done."
